d20ebc43b093daf3dd0c7a5c6d3cccaa16d9ccc1
Alison Williams <apw5011@gmail.com>
2020-12-14 21:51:27 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,22 +1,22 @@
 module Players 
   class Computer < Player 
-    #def move(board)
-      #if !board.taken?("5")
-        #"5"
-      #elsif board.taken?("5")
-        #"1"
-      #elsif board.taken?("5") && board.taken?("1")
-        #"9"
-      #elsif board.taken?("5") && board.taken?("1") && board.taken?("9")
-        #"3"
-      #elsif board.taken?("5") && board.taken?("1") && board.taken?("9") && board.taken?("3")
-        #"7"
-      #elsif board.taken?("5") && board.taken?("1") && board.taken?("9") && board.taken?("3") && board.taken?("7")
-        #"2"
-      #elsif board.taken?("5") && board.taken?("1") && board.taken?("9") && board.taken?("3") && board.taken?("7") && board.taken("2")
-        
-      #end 
-    #end 
+    def move(board)
+      if !board.taken?("5")
+        "5"
+      elsif board.taken?("5")
+        "1"
+      elsif board.taken?("5") && board.taken?("1")
+        "9"
+      elsif board.taken?("5") && board.taken?("1") && board.taken?("9")
+        "3"
+      elsif board.taken?("5") && board.taken?("1") && board.taken?("9") && board.taken?("3")
+        "7"
+      elsif board.taken?("5") && board.taken?("1") && board.taken?("9") && board.taken?("3") && board.taken?("7")
+        "2"
+      elsif board.taken?("5") && board.taken?("1") && board.taken?("9") && board.taken?("3") && board.taken?("7") && board.taken?("2")
+        "4"
+      end 
+    end 
     
     #def computer_move(board)
       #corner(board) || side(board) 
