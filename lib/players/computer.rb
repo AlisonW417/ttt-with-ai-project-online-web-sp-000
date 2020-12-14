9bc9d0eb2858ec6fe57be3d9699a463ddf83d13b
Alison Williams <apw5011@gmail.com>
2020-12-14 21:42:25 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,14 +3,18 @@ module Players
     def move(board)
       if !board.taken?("5")
         "5"
-      else 
+      elsif board.taken?("5")
+        "1"
+      elsif board.taken?("5") && board.taken("1")
+        "9"
+      elsif board.taken?("5") && board.taken("1") && board.taken
         computer_move(board)
       end 
     end 
     
-    def computer_move(board)
-      corner(board) || side(board) 
-    end 
+    #def computer_move(board)
+      #corner(board) || side(board) 
+    #end 
     
     ##def corner(board)
      ## if board.taken?("5")
